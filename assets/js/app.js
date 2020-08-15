b11eb910c539a779955c6f7cbc2b4efa470ce689
Jakob <jakob@folkdatorn.se>
2020-08-15 02:26:46 +0200

    Bimse audio + audio player

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,11 +29,47 @@ const renderReviews = () => {
 const sendEmail = () => {
   const subject = document.querySelector('#subject').value
   const message = document.querySelector('#message').value
-
-  console.log(subject)
-  console.log(message)
-
   window.location.href = 'mailto:maria@eyeblick.se?subject=' + subject + '&body=' + message + '%0D%0A %0D%0A'
+}
+
+let playing = false
+const audio = new window.Audio('./assets/audio/bimse.m4a');
+
+const togglePlayBack = async () => {
+  playing = !playing
+  if (audio.ended) {
+    await replay()
+  }
+
+  const element = document.getElementById('playPause')
+  const slider = document.getElementById('slider')
+  if (playing) {
+    element.src = './assets/img/pause.svg'
+    slider.style.transitionDuration = audio.duration - audio.currentTime + 's'
+    slider.style.width = '100%'
+    audio.play()
+  } else {
+    element.src = './assets/img/play.svg'
+    slider.style.width = (audio.currentTime / audio.duration) * 100 + '%'
+    audio.pause()
+  }
+  element.style.animationName = 'bounceIn'
+  window.setTimeout(() => {
+    element.style.animationName = ''
+  }, 200)
+}
+
+const replay = () => {
+  const slider = document.getElementById('slider')
+  audio.currentTime = 0
+  slider.style.transition = 0
+  slider.style.width = 0
+  if (playing) {
+    window.setTimeout(() => {
+      slider.style.transitionDuration = audio.duration - audio.currentTime + 's'
+      slider.style.width = '100%'
+    }, 1)
+  }
 }
 
 calculateAspect()
